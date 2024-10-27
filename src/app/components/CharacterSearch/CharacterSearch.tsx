ee4ffca019c2a765b4f545823c4c399efdb7b3f8
Max Logvinov <maxlogvinov93@gmail.com>
2024-10-28 02:01:16 +0300

    add filters status and species

--- src/app/components/CharacterSearch/CharacterSearch.tsx
+++ src/app/components/CharacterSearch/CharacterSearch.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCharacters } from '../../servises/thunks/charactersThunk';
@@ -11,35 +9,90 @@ const DEBOUNCE_DELAY = 1000;
 
 const CharacterSearch: React.FC = () => {
   const [name, setName] = useState('');
+  const [status, setStatus] = useState<string | undefined>(undefined);
+  const [speciesOptions, setSpeciesOptions] = useState<string[]>([]);
+  const [species, setSpecies] = useState<string | undefined>(undefined);
+
   const dispatch = useDispatch<AppDispatch>();
   const { characters, loading, error } = useSelector((state: RootState) => state.characters);
 
+  // Отправка запроса с дебаунсом
   useEffect(() => {
     const handler = setTimeout(() => {
-      if (name.trim()) {
-        dispatch(fetchCharacters(name));
+      if (name.trim() || status || species) {
+        dispatch(fetchCharacters({ name, status, species: species }));
       }
     }, DEBOUNCE_DELAY);
 
-    return () => {
-      clearTimeout(handler);
-    };
-  }, [name, dispatch]);
+    return () => clearTimeout(handler);
+  }, [name, status, species, dispatch]);
+
+  // Получение уникальных видов из данных
+  useEffect(() => {
+    if (characters.length > 0) {
+      const uniqueSpecies = Array.from(new Set(characters.map(character => character.species)));
+      setSpeciesOptions(uniqueSpecies);
+    }
+  }, [characters]);
+
+  // Обработчик для сброса статуса
+  const handleStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value === 'reset' ? undefined : e.target.value;
+    setStatus(value);
+  };
+
+  // Обработчик для сброса вида
+  const handleSpeciesChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value === 'reset' ? undefined : e.target.value;
+    setSpecies(value);
+  };
+
+  console.log(characters);
 
   return (
-    <div className="border-2 border-white w-3/6 rounded-2xl font-get-schwifty">
-      <div className="flex items-center flex-col p-6 ">
-        <h1 className="text-3xl self-start ">The universe of Rick and Morty</h1>
+    <div className="border-2 border-white w-3/6 rounded-2xl font-get-schwifty bg-inherit">
+      <div className="flex items-center flex-col p-6 gap-4 bg-inherit">
+        <h1 className="text-4xl self-start">The universe of Rick and Morty</h1>
         <label className="flex flex-col w-full">
           Character name
           <input
             type="text"
             value={name}
-            className="text-black"
+            className="pl-1.5 rounded-lg bg-inherit border border-solid border-gray-300"
             onChange={e => setName(e.target.value)}
             placeholder="Enter the character's name"
           />
         </label>
+        <div className="flex items-center justify-between w-full gap-8 bg-inherit">
+          <label className="flex flex-col w-full bg-inherit">
+            Alive?
+            <select
+              className="pl-1.5 rounded-lg bg-inherit border border-solid border-gray-300"
+              value={status ?? 'reset'}
+              onChange={handleStatusChange}
+            >
+              <option value="reset">select status / reset</option>
+              <option value="Alive">Alive</option>
+              <option value="Dead">Dead</option>
+              <option value="unknown">unknown</option>
+            </select>
+          </label>
+          <label className="flex flex-col w-full bg-inherit">
+            Species
+            <select
+              className="pl-1.5 rounded-lg bg-inherit border border-solid border-gray-300"
+              value={species ?? 'reset'}
+              onChange={handleSpeciesChange}
+            >
+              <option value="reset">select species / reset</option>
+              {speciesOptions.map(species => (
+                <option key={species} value={species}>
+                  {species}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
       </div>
 
       {loading && <p>Loading...</p>}
--- src/app/servises/thunks/charactersThunk.ts
+++ src/app/servises/thunks/charactersThunk.ts
@@ -2,16 +2,20 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Character } from '@/app/types/types';
 
-export const fetchCharacters = createAsyncThunk<Character[], string, { rejectValue: string }>(
-  'characters/fetchCharacters',
-  async (name, { rejectWithValue }) => {
-    try {
-      const response = await axios.get(`https://rickandmortyapi.com/api/character?name=${name}`);
-      console.log(response.data);
-      return response.data.results;
-      //eslint-disable-next-line
-    } catch (error: any) {
-      return rejectWithValue(error.message || 'Failed to fetch characters');
-    }
+export const fetchCharacters = createAsyncThunk<
+  Character[],
+  { name: string; status?: string; species?: string },
+  { rejectValue: string }
+>('characters/fetchCharacters', async ({ name, status, species }, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(
+      `https://rickandmortyapi.com/api/character?name=${name}${status ? `&status=${status}` : ''}${
+        species ? `&species=${species}` : ''
+      }`
+    );
+    return response.data.results;
+    //eslint-disable-next-line
+  } catch (error: any) {
+    return rejectWithValue(error.message || 'Failed to fetch characters');
   }
-);
+});
